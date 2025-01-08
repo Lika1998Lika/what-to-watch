2710a6635752a3b68327895a1aa30da8a0611e50
Anzhelika <anzhelika_vanuyto@mail.ru>
2025-01-08 23:58:18 +0500

    axios package configured, redux-thunk enabled

--- src/Types/state.ts
+++ src/Types/state.ts
@@ -1,4 +1,4 @@
-import { store } from "../Store/store";
+import { store } from "../Store";
 
 //работа с конкретным объектом хранилища, который содержит информ. о всех ключах и т.д
 export type RootState = ReturnType<typeof store.getState>;
